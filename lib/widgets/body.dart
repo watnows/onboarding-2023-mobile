8845ac83a9dd46663b428f0cccc3a4663671069e
sisosyunya <syunyasisotto@gmail.com>
2023-04-12 15:46:06 +0900

    [add]list view

--- lib/widgets/body.dart
+++ lib/widgets/body.dart
@@ -12,7 +12,7 @@ class _TwitterBodyState extends State<TwitterBody> {
   String _todo = "";
   String inputText = "";
   //ツイートを格納するリスト
-  List _tweetList = [];
+ final List _tweetList = [TweetBody(tweetText: "テストだよ")];
 
   @override
   Widget build(BuildContext context) {
@@ -32,19 +32,25 @@ class _TwitterBodyState extends State<TwitterBody> {
         ),
         const Gap(30),
         Row(
-          // crossAxisAlignment: CrossAxisAlignment.end,
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
             ElevatedButton(
               onPressed: () {
-                //ツイートする時の関数だよ
-                setState(() {
-                  _todo = inputText;
-                });
+                if (inputText != "") {
+                  //ツイートする時の関数だよ
+                  // setState(() {
+                  //   _todo = inputText;
+                  // });
+                  //ツイートをリストに追加する
+                  setState(() {
+                    _tweetList.add(TweetBody(tweetText: inputText));
+                  });
+                }
               },
+              //ツイートすると
               style: ElevatedButton.styleFrom(
-                shape:
-                    RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
               ),
               //文字だよ
               child: const Text('ツイートする'),
@@ -53,6 +59,15 @@ class _TwitterBodyState extends State<TwitterBody> {
           ],
         ),
         TweetBody(tweetText: _todo),
+        //配列の中身があれば、それを表示する
+        (_tweetList.length > 0)
+        ? ListView.builder(
+            shrinkWrap: true,
+            itemCount: _tweetList.length,
+            itemBuilder: (context, index) {
+              return  _tweetList[index];
+            })
+        : Container(height: 100),
       ],
     );
   }
